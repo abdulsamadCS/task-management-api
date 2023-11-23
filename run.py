31d01e0d33fc5f4a31d240dfa1389ec3857a9fd2
Abdul Samad <abdulsamad.cs15@gmail.com>
2023-11-24 02:59:58 +0500

    Update run.py - Add blueprints - Add code to create table if not exists

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
+from flask import Flask
 
+from app.database import Database
+from app.views import tasks_bp
+
+app = Flask(__name__)
+
+# Create an instance of the Database class
+database = Database()
+
+# Register the 'tasks' blueprint
+app.register_blueprint(tasks_bp)
+
+# Create the 'tasks' table if it doesn't exist
+database.create_task_table()
+
+if __name__ == "__main__":
+    app.run(debug=True)
