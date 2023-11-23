c5469b1e3b5cf601a30b20e0abccf0e11c07fb1d
Abdul Samad <abdulsamad.cs15@gmail.com>
2023-11-24 01:26:43 +0500

    Create db config and helper finctions

---
+++ app/database.py
@@ -0,0 +1,29 @@
+import os
+
+import mysql.connector
+
+
+class Database:
+    def __init__(self):
+        self.db_config = {
+            "host": os.getenv("DB_HOSTNAME"),
+            "user": os.getenv("DB_NAME"),
+            "password": os.getenv("DB_USER"),
+            "database": os.getenv("DB_PASSWORD"),
+        }
+
+    def execute_query(self, query, params=None, fetchone=False):
+        try:
+            conn = mysql.connector.connect(**self.db_config)
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(query, params)
+            conn.commit()
+            result = cursor.fetchone() if fetchone else cursor.fetchall()
+            return result
+        except mysql.connector.Error as e:
+            print(f"Error: {e}")
+        finally:
+            if "cursor" in locals() and cursor is not None:
+                cursor.close()
+            if "conn" in locals() and conn is not None:
+                conn.close()
